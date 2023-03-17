2c281c8d3ad6d576751170810daf7e1594f7d98e
TrubitsynEgor <trubitsynwork74@gmail.com>
2023-03-17 12:43:31 +0500

    +Modal

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { useState, useMemo } from "react";
 import CreateForm from "./components/CreateForm";
 import PostFilter from "./components/PostFilter";
 import Posts from "./components/Posts";
+import MyButton from "./components/UI/buttons/MyButton";
 import MyInput from "./components/UI/inputs/MyInput";
+import MyModal from "./components/UI/modals/MyModal";
 import NotFound from "./components/UI/not-found/NotFound";
 import SortSelect from "./components/UI/selects/SortSelect";
 import './styles/App.css'
@@ -19,9 +21,11 @@ const App = () => {
     sort: '',
     query: '',
   })
+  const [modal, setModal] = useState(false)
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
+    setModal(false)
   }
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
@@ -43,7 +47,11 @@ const App = () => {
   return (
 
     <div className="App">
-      <CreateForm createPost={createPost} />
+      <MyButton style={{ margin: '15px 0' }} onClick={() => setModal(true)}>Create post!</MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <CreateForm createPost={createPost} />
+      </MyModal>
+
       <PostFilter filter={filter} setFilter={setFilter} />
       <Posts removePost={removePost} posts={sortedAndSearchedPosts} title='Posts List JavaScript:' />
 
